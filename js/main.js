e705366ed7bda08a2f08c6783f1b8918e742446a
PedroPereiradaCostaJr <pedro.juniorp@hotmail.com>
2023-10-14 18:47:53 -0300

    Update

---
+++ js/main.js
@@ -0,0 +1,21 @@
+const tabItems = document.querySelectorAll(".tab-item");
+const tabContentItems = document.querySelectorAll(".tab-content-item");
+
+function selectItem(e) {
+  removeBorder();
+  removeShow();
+  //borda na guia atual
+  this.classList.add("tab-border");
+  // pega o conteúdo do item pelo DOM
+  const tabContentItem = document.querySelector(`#${this.id}-content`);
+  tabContentItem.classList.add("show");
+}
+
+function removeBorder() {
+  tabItems.forEach((item) => item.classList.remove("tab-border"));
+}
+function removeShow() {
+  tabContentItems.forEach((item) => item.classList.remove("show"));
+}
+//evento de click
+tabItems.forEach((item) => item.addEventListener("click", selectItem));
